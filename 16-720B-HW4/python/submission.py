7aeb579350e1f0ab3f0edbc91318889b352a926c
Du <hzsydy@foxmail.com>
2018-11-05 20:42:26 -0500

    2-1

--- 16-720B-HW4/python/submission.py
+++ 16-720B-HW4/python/submission.py
@@ -4,7 +4,9 @@ Replace 'pass' by your implementation.
 """
 
 # Insert your package here
-
+import numpy as np
+from helper import *
+import cv2
 
 '''
 Q2.1: Eight Point Algorithm
@@ -13,9 +15,27 @@ Q2.1: Eight Point Algorithm
             M, a scalar parameter computed as max (imwidth, imheight)
     Output: F, the fundamental matrix
 '''
+
+
 def eightpoint(pts1, pts2, M):
     # Replace pass by your implementation
-    pass
+    pts1_scaled = pts1.astype(np.float) / M
+    pts2_scaled = pts2.astype(np.float) / M
+
+    n = pts1.shape[0]
+    A = np.zeros((n, 9))
+    A[:, 2::3] = 1
+    A[:, 1::3] = pts1_scaled[:, 1:2].copy()
+    A[:, 0::3] = pts1_scaled[:, 0:1].copy()
+    A[:, 0:3] *= pts2_scaled[:, 0:1]
+    A[:, 3:6] *= pts2_scaled[:, 1:2]
+
+    u, s, vh = np.linalg.svd(A.T.dot(A))
+    f = u[:, -1].reshape((3, 3))
+
+    f = refineF(f, pts1_scaled, pts2_scaled)
+    t = np.diag([1./M, 1./M, 1]) # the scalar matrix
+    return t.dot(f).dot(t)
 
 
 '''
@@ -25,6 +45,8 @@ Q2.2: Seven Point Algorithm
             M, a scalar parameter computed as max (imwidth, imheight)
     Output: Farray, a list of estimated fundamental matrix.
 '''
+
+
 def sevenpoint(pts1, pts2, M):
     # Replace pass by your implementation
     pass
@@ -37,6 +59,8 @@ Q3.1: Compute the essential matrix E.
             K2, internal camera calibration matrix of camera 2
     Output: E, the essential matrix
 '''
+
+
 def essentialMatrix(F, K1, K2):
     # Replace pass by your implementation
     pass
@@ -51,6 +75,8 @@ Q3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
     Output: P, the Nx3 matrix with the corresponding 3D points per row
             err, the reprojection error.
 '''
+
+
 def triangulate(C1, pts1, C2, pts2):
     # Replace pass by your implementation
     pass
@@ -67,9 +93,12 @@ Q4.1: 3D visualization of the temple images.
             y2, y-coordinates of the pixel on im2
 
 '''
+
+
 def epipolarCorrespondence(im1, im2, F, x1, y1):
     # Replace pass by your implementation
     pass
+
 
 '''
 Q5.1: RANSAC method.
@@ -79,27 +108,36 @@ Q5.1: RANSAC method.
     Output: F, the fundamental matrix
             inliers, Nx1 bool vector set to true for inliers
 '''
+
+
 def ransacF(pts1, pts2, M):
     # Replace pass by your implementation
     pass
+
 
 '''
 Q5.2: Rodrigues formula.
     Input:  r, a 3x1 vector
     Output: R, a rotation matrix
 '''
+
+
 def rodrigues(r):
     # Replace pass by your implementation
     pass
+
 
 '''
 Q5.2: Inverse Rodrigues formula.
     Input:  R, a rotation matrix
     Output: r, a 3x1 vector
 '''
+
+
 def invRodrigues(R):
     # Replace pass by your implementation
     pass
+
 
 '''
 Q5.3: Rodrigues residual.
@@ -111,9 +149,12 @@ Q5.3: Rodrigues residual.
             x, the flattened concatenationg of P, r2, and t2.
     Output: residuals, 4N x 1 vector, the difference between original and estimated projections
 '''
+
+
 def rodriguesResidual(K1, M1, p1, K2, p2, x):
     # Replace pass by your implementation
     pass
+
 
 '''
 Q5.3 Bundle adjustment.
@@ -127,6 +168,27 @@ Q5.3 Bundle adjustment.
     Output: M2, the optimized extrinsics of camera 1
             P2, the optimized 3D coordinates of points
 '''
+
+
 def bundleAdjustment(K1, M1, p1, K2, M2_init, p2, P_init):
     # Replace pass by your implementation
     pass
+
+
+def main():
+    im1 = cv2.imread('../data/im1.png')
+    im2 = cv2.imread('../data/im2.png')
+    # test 2.1
+    data = np.load('../data/some_corresp.npz')
+    pts1 = data['pts1']
+    pts2 = data['pts2']
+    M = max(*im1.shape)
+
+    f = eightpoint(pts1, pts2, M)
+    print(f)
+    np.savez('q2_1.npz', F=f, M=M)
+    displayEpipolarF(im1, im2, f)
+
+
+if __name__ == '__main__':
+    main()
